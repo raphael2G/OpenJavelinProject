1213ad34b493560c67307e16d6c7267f51fd1d4c
Ishmael <darmfiel@andrew.cmu.edu>
2024-07-30 17:42:40 -0400

    removed elements in header, added toast to signout

--- frontend/src/components/header/header.js
+++ frontend/src/components/header/header.js
@@ -1,33 +1,51 @@
-import './header.css'
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+import { useToast } from "@/components/ui/use-toast"
 
 
 
 export default function Header ({isLoggedin, auth}) {
     const navigate = useNavigate();
+    const { toast } = useToast()
+
 
     const onSignOut = () => {
         console.log("Signing Out: ", auth);
         auth.signOut();
         navigate('/');
+        toast({
+            title: "Successfully signed out"
+          })
     }
 
     return (
         <>
-            <header className='mainContainer'>
-                <ul className = 'ul'>
-                    <i><Link to="/">Home</Link></i>
-                    <i><Link to="/about">About</Link></i>
+            <header className="bg-white shadow-sm">
+            <div className="container mx-auto flex justify-between items-center p-4">
+                <Link to="/"><div className="text-xl font-bold"> Open Javelin Project</div></Link>
+                <ul className="flex space-x-6">
+                    <li className="hover:text-blue-500">
+                        <Link to="/discoveries">Discoveries</Link>
+                    </li>
                     {isLoggedin ? (
                         <>
-                            <i><Link to="/profile">Profile</Link></i>
-                            <i> <button onClick={onSignOut}>Sign Out</button> </i>
+                        <li className="hover:text-blue-500">
+                            <Link to="/profile">Profile</Link>
+                        </li>
+                        <li
+                            className="hover:text-blue-500 cursor-pointer"
+                            onClick={onSignOut}
+                        >
+                            Sign Out
+                        </li>
                         </>
                     ) : (
-                        <i> <Link to="/login">Login</Link> </i>
+                        <li className="hover:text-blue-500">
+                        <Link to="/login">Login</Link>
+                        </li>
                     )}
                 </ul>
+            </div>
             </header>
         </>
     );
