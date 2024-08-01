40d4118151304558955c6972ca62baa7467a3cbc
Ishmael <darmfiel@andrew.cmu.edu>
2024-08-01 19:23:09 -0400

    changed API routes to new database

--- frontend/src/components/profile/profile.js
+++ frontend/src/components/profile/profile.js
@@ -13,15 +13,42 @@ export default function Profile({ uid }) {
 
     useEffect(() => {
         AOS.init({ duration: 1000, once: false });
-        axios.get(`http://localhost:5001/api/userData/uid/${auth?.currentUser?.uid}`)
-            .then(res => {
-                console.log(res.data);
-                setSubmissions(res.data);
-            })
-            .catch(err => {
-                console.log(err);
-            });
-    }, [uid]);
+        console.log(JSON.parse(localStorage.getItem('submissions')))
+    }, []);
+
+    useEffect(() => {
+        setTimeout(() => {
+            if (!auth.currentUser) {
+                navigate('/login');
+                console.log(auth.currentUser);
+            } else {
+                if (!selectedSubmission && JSON.parse(localStorage.getItem('submissions')).length > 0) {
+                    console.log("found submissions in local storage");
+                    setSubmissions(JSON.parse(localStorage.getItem('submissions')));
+                } else if (!selectedSubmission) {
+                    console.log("failed to find submissions in local storage. fetching submissions from serverless function");
+                    console.log(JSON.parse(localStorage.getItem('submissions')))
+                    axios.get(process.env.REACT_APP_MONGODB_SEVERLESS_QUERY_BY_UID, 
+                        {params: {
+                        uid: auth?.currentUser?.uid
+                        }}
+                    )
+                    .then(res => {
+                        console.log(res.data);
+                        setSubmissions(res.data);
+                        localStorage.setItem('submissions', JSON.stringify(res.data));
+                        console.log("local storage", JSON.parse(localStorage.getItem('submissions')))
+                    })
+                    .catch(err => {
+                        console.log(err);
+                    });
+                }
+            }
+        }, 1000);
+    }, [auth]);
+
+
+
 
     const handleSquareClick = (submission) => {
         if (selectedSubmission && submission?.createdAt === selectedSubmission?.createdAt) {
@@ -32,7 +59,7 @@ export default function Profile({ uid }) {
     };
 
     return (
-        <div className="bg-gray-100 min-h-screen p-4">
+        <div className="bg-gray-100 min-h-screen p-4 pt-[150px]">
             <div className="container mx-auto">
                 <div className="bg-white shadow-md rounded-lg p-6 mb-8">
                     <h1 className="text-4xl font-bold text-center mb-4">Submission History</h1>
@@ -46,7 +73,7 @@ export default function Profile({ uid }) {
                                 onClick={() => handleSquareClick(submission)}
                                 className={`yearSquare cursor-pointer p-2 rounded-lg ${selectedSubmission?.createdAt === submission.createdAt ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
                             >
-                                {submission.createdAt.substring(0, 4)}
+                                {submission?.createdAt?.substring(0, 4)}
                             </div>
                         ))}
                     </div>
